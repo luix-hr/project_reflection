c56f41f6de21900d248dc55697d916e5cdaebe4e
Luiz Henrique <luizh.rocha16@gmail.com>
2024-05-28 10:55:11 -0300

    Usando a classe Class

--- src/main/java/com/tceweb/project_reflection/refl/Transformator.java
+++ src/main/java/com/tceweb/project_reflection/refl/Transformator.java
@@ -5,7 +5,6 @@ public class Transformator {
     public <I, O> transform(I input) throws ClassNotFoundException {
         Class<?> source = input.getClass();
         Class<?> target = source.forName(source.getClass() + "DTO");
-
     }
 
 }
