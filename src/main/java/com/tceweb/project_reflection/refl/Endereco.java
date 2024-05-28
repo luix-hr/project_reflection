68583ea28a4512865f28caec0f96baa44078a79b
Luiz Henrique <luizh.rocha16@gmail.com>
2024-05-28 12:06:52 -0300

    Teste shouldNotTransform

---
+++ src/main/java/com/tceweb/project_reflection/refl/Endereco.java
@@ -0,0 +1,12 @@
+package com.tceweb.project_reflection.refl;
+
+public class Endereco {
+
+    private String rua;
+    private Integer numero;
+
+    public Endereco(String rua, Integer numero) {
+        this.rua = rua;
+        this.numero = numero;
+    }
+}
--- src/test/java/com/tceweb/project_reflection/refl/TransformatorTest.java
+++ src/test/java/com/tceweb/project_reflection/refl/TransformatorTest.java
@@ -8,6 +8,7 @@ import java.lang.reflect.InvocationTargetException;
 public class TransformatorTest {
 
     Pessoa pessoa = new Pessoa(1, "Joao", "1234");
+    Endereco endereco = new Endereco("10", 20);
 
     @Test
     public void shouldTransform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
@@ -18,6 +19,17 @@ public class TransformatorTest {
         Assertions.assertInstanceOf(PessoaDTO.class, pessoaDTO);
         Assertions.assertEquals(pessoa.getNome(), pessoaDTO.getNome());
         Assertions.assertEquals(pessoa.getCpf(), pessoaDTO.getCpf());
+    }
+
+    @Test
+    public void shouldNotTransform(){
+
+       Assertions.assertThrows(ClassNotFoundException.class, () -> {
+           Transformator transformator = new Transformator();
+           transformator.transform(endereco);
+       });
+
+
     }
 
 }
